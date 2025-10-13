31dd1ddfea3a2a7756525622aa10b78f09c4fd79
reo101 <pavel.atanasov2001@gmail.com>
2025-10-13 18:26:10 +0300

    wip

--- flake.nix
+++ flake.nix
@@ -2,121 +2,126 @@
   description = "sosumi-docs-downloader";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixos-modules.url = "github:metacraft-labs/nixos-modules";
+    nixpkgs.follows = "nixos-modules/nixpkgs-unstable";
+    flake-parts.follows = "nixos-modules/flake-parts";
+    fenix.follows = "nixos-modules/fenix";
+    crane.follows = "nixos-modules/crane";
     git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    git-hooks,
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    checks = forAllSystems (system: let
-      pkgs = import nixpkgs { inherit system; };
-      preCommit = git-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          # Rust formatting and linting
-          rustfmt = {
-            enable = true;
-            name = "rustfmt";
-            entry = "rustfmt --edition 2021";
-            language = "system";
-            pass_filenames = true;
-            files = "\\.rs$";
-          };
-          clippy = {
-            enable = true;
-            name = "clippy";
-            entry = "cargo clippy -- -D warnings";
-            language = "system";
-            pass_filenames = false;
-          };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-          # TOML formatting
-          taplo-fmt = {
-            enable = true;
-            name = "taplo fmt";
-            entry = "taplo fmt";
-            language = "system";
-            pass_filenames = true;
-            files = "\\.toml$";
-          };
-        };
-        tools = {
-          rustfmt = pkgs.rustfmt;
-          taplo = pkgs.taplo;
-        };
-      };
+    perSystem = { inputs', self', lib, pkgs, system, ... }: let
+      rustToolchain = inputs'.fenix.packages.stable.toolchain;
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain inputs.fenix.packages.${system}.stable.toolchain;
     in {
-      pre-commit-check = preCommit;
-    });
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [];
+      };
 
-    packages = forAllSystems (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
-        sosumi-downloader = pkgs.rustPlatform.buildRustPackage {
-          pname = "sosumi-docs-downloader";
-          version = "0.1.0";
+      checks = let
+        preCommit = inputs.git-hooks.lib.${system}.run {
           src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          buildInputs = [];
-          nativeBuildInputs = [];
+          hooks = {
+            # Rust formatting and linting
+            rustfmt = {
+              enable = true;
+              name = "rustfmt";
+              entry = "rustfmt --edition 2021";
+              language = "system";
+              pass_filenames = true;
+              files = "\\.rs$";
+            };
+            clippy = {
+              enable = true;
+              name = "clippy";
+              entry = "cargo clippy -- -D warnings";
+              language = "system";
+              pass_filenames = false;
+            };
+
+            # TOML formatting
+            taplo-fmt = {
+              enable = true;
+              name = "taplo fmt";
+              entry = "taplo fmt";
+              language = "system";
+              pass_filenames = true;
+              files = "\\.toml$";
+            };
+          };
+          tools = {
+            rustfmt = pkgs.rustfmt;
+            taplo = pkgs.taplo;
+          };
         };
       in {
-        sosumi-docs-downloader = sosumi-downloader;
-        default = sosumi-downloader;
-      }
-    );
-
-    apps = forAllSystems (system: {
-      sosumi-docs-downloader = {
-        type = "app";
-        program = "${self.packages.${system}.sosumi-docs-downloader}/bin/sosumi-docs-downloader";
-      };
-      default = self.apps.${system}.sosumi-docs-downloader;
-    });
-
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ rust-overlay.overlays.default ];
+        pre-commit-check = preCommit;
       };
 
-      packages = [
-        # Rust toolchain
-        (pkgs.rust-bin.stable.latest.default.override {
-          extensions = ["rustfmt" "clippy"];
-        })
+      packages = let
+      in {
+        sosumi-docs-downloader = craneLib.buildPackage {
+          src = ./.;
 
-        # Basic development tools
-        pkgs.git
-        pkgs.just
-        pkgs.taplo
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.gcc
+            # pkgs.stdenv.cc.cc
+            # pkgs.stdenv.cc.bintools
+          ];
 
-        # Cargo tools
-        pkgs.cargo-outdated
-        pkgs.cargo-watch
-      ];
-
-    in {
-      default = pkgs.mkShell {
-        buildInputs = packages ++ self.checks.${system}.pre-commit-check.enabledPackages;
-
-        shellHook = ''
-          # Install git pre-commit hook invoking our Nix-defined hooks
-          ${self.checks.${system}.pre-commit-check.shellHook}
-          echo "Sosumi docs downloader development environment loaded"
-        '';
+          meta = {
+            description = "Downloader for Sosumi documentation";
+            license = lib.licenses.mit;
+            platforms = lib.platforms.unix;
+            mainProgram = "sosumi-docs-downloader";
+          };
+        };
+        default = self'.packages.sosumi-docs-downloader;
       };
-    });
+
+      apps = {
+        sosumi-docs-downloader = {
+          type = "app";
+          program = lib.getExe self'.packages.sosumi-docs-downloader;
+        };
+        default = self'.apps.sosumi-docs-downloader;
+      };
+
+      devShells = let
+        packages = [
+          # TODO: Rust toolchain
+          # rustPlatform.toolchain
+
+          # Basic development tools
+          pkgs.git
+          pkgs.just
+          pkgs.taplo
+
+          # Cargo tools
+          pkgs.cargo-outdated
+          pkgs.cargo-watch
+        ];
+      in {
+        default = pkgs.mkShell {
+          buildInputs = packages ++ self'.checks.pre-commit-check.enabledPackages;
+
+          shellHook = ''
+            # Install git pre-commit hook invoking our Nix-defined hooks
+            ${self'.checks.pre-commit-check.shellHook}
+            echo "Sosumi docs downloader development environment loaded"
+          '';
+        };
+      };
+    };
   };
 }
